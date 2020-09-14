47e898cef8b4d59acce18bfab917fdd5059a48fc
Samual <samual@finnigan.me>
2020-09-13 22:33:49 -0700

    Added multiply functionality, tests, and readme update

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 Simple Python calculator
 
 Add (+): Adds two given numbers and prints the result
+
 Subtract (-): Subtracts two given numbers, in given order, and prints the result
+
+Multiply (*): Multiplies two given numbers, and prints the result
--- SimpleCalc.py
+++ SimpleCalc.py
@@ -1,19 +1,25 @@
+import math
+
 def Add(a, b):
 	return a + b
 def TestAdd():
-	assert Add(12.0, 7.0) == 19.0
+	assert Add(12.0, 7.4) == 19.4
 	assert Add(5.0, 36.0) == 41.0
 	assert Add(61.0, 532.0) == 593.0
 
 def Subtract(a, b):
 	return a - b
 def TestSubtract():
-	assert Subtract(12.0, 7.0) == 5.0
+	assert Subtract(12.0, 7.4) == 4.6
 	assert Subtract(5.0, 36.0) == -31.0
 	assert Subtract(61.0, 532.0) == -471.0
 
 def Multiply(a, b):
-	return "TODO"
+	return a * b
+def TestMultiply():
+	assert math.isclose(Multiply(12.0, 7.4), 88.8)
+	assert math.isclose(Multiply(5.0, 36.0), 180.0)
+	assert math.isclose(Multiply(61.0, 532.0), 32452.0)
 
 def Divide(a, b):
 	return "TODO"
@@ -21,8 +27,8 @@ def Divide(a, b):
 
 # start execution #
 
-#testMode = True
-testMode = False
+testMode = True
+#testMode = False
 
 if (not testMode):
 	num1 = float(input("Enter a number: "))
@@ -46,5 +52,6 @@ if (not testMode):
 else:
 	TestAdd()
 	TestSubtract()
+	TestMultiply()
 
 	print("All tests passed")
